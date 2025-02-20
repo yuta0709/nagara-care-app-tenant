fd0d9317582278942189985b82d4fee09263c636
yuta0709 <58354812+yuta0709@users.noreply.github.com>
2025-02-20 22:32:33 +0900

    デプロイ用の設定

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from "@react-router/dev/config";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  buildEnd: (args) => {
+    if (!args.viteConfig.isProduction) return;
+    const buildPath = args.viteConfig.build.outDir;
+    copyFileSync(join(buildPath, "index.html"), join(buildPath, "404.html"));
+  },
 } satisfies Config;
